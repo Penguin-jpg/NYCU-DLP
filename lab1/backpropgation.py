b298b4e7740e2c1d8a276e1df5643e0c5de3af5f
Penguin.jpg <sky026912@gmail.com>
2024-03-15 14:27:41 +0800

    add testing

--- lab1/backpropgation.py
+++ lab1/backpropgation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # dataset generator function
@@ -49,11 +50,16 @@ def derivative_relu(x):
     return np.where(x > 0, 1, 0)
 
 
-# loss function (y: ground-truth, y_hat: prediction)
+# loss function (y: ground-truth, y_hat: prediction) and derivative
 # because this is a classification task, we use binary cross-entropy
 def binary_cross_entropy(y, y_hat):
     # calculate like this because input one data point at a time
     return -(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+
+
+def derivative_binary_cross_entropy(y, y_hat):
+    # dL/d(y_hat) = -(y/y_hat - (1-y)/(1-y_hat)) = -(y-y_hat)/(y_hat * (1-y_hat))
+    return -(y / y_hat - (1 - y) / (1 - y_hat))
 
 
 class Network:
@@ -97,25 +103,26 @@ class Network:
         self.values["x"] = x
 
         # for each layer, calculate a=act(z=w^Tx+b)
-        # z1 = np.dot(self.input_w, x) + self.input_b  # shape: (4, 1)
+        # z1 = np.dot(self.w1, x) + self.b1  # shape: (4, 1)
         # a1 = self.act(z1)
         # self.values["z1"] = z1
         # self.values["a1"] = a1
 
-        # z2 = np.dot(self.hidden1_w, self.a1) + self.hidden1_b  # shape: (4, 1)
+        # z2 = np.dot(self.w2, a1) + self.b2  # shape: (4, 1)
         # a2 = self.act(z2)
         # self.values["z2"] = z2
         # self.values["a2"] = a2
 
-        # z3 = np.dot(self.hidden2_w, self.a2) + self.hidden2_b  # shape: (4, 1)
+        # z3 = np.dot(self.w3, a2) + self.b3  # shape: (4, 1)
         # a3 = self.act(z3)
         # self.values["z3"] = z3
         # self.values["a3"] = a3
 
-        # z4 = np.dot(self.output_w, self.a3) + self.output_b  # shape: (1, 1)
+        # z4 = np.dot(self.w4, a3) + self.b4  # shape: (1, 1)
         # y = self.act(z4)
         # self.values["z4"] = z4
         # self.values["a4"] = y
+
         z1 = np.dot(self.w1, x)  # shape: (4, 1)
         a1 = self.act(z1)
         self.values["z1"] = z1
@@ -141,50 +148,79 @@ class Network:
     def backward(self, y, y_hat):
         # backprogagation starts from output layer (using chain rule)
 
-        # the graident of output layer: dL/d(y_hat) * d(y_hat)/d(z4) * d(z4)/d(w4) = dL/d(w4)
-        # derivative of binary cross entropy w.r.t y_hat (dL/d(y_hat)):
-        # dL/d(y_hat) = -(y/y_hat - (1-y)/(1-y_hat)) = -(y-y_hat)/(y_hat * (1-y_hat))
-        d_L_d_y_hat = -(y / y_hat - (1 - y) / (1 - y_hat))  # y_hat = a4, shape: (1, 1)
+        # the graident of output layer:
+        # dL/d(w4) = dL/d(y_hat) * d(y_hat)/d(z4) * d(z4)/d(w4)
+        # dL/d(b4) = dL/d(y_hat) * d(y_hat)/d(z4) * d(z4)/d(b4)
+        d_L_d_y_hat = derivative_binary_cross_entropy(y, y_hat)  # y_hat = a4, shape: (1, 1)
         d_y_hat_d_z4 = self.derivative_act(self.values["z4"])  # d(a4)/d(z4)=act'(z4), shape: (1, 1)
         d_z4_d_w4 = self.values["a3"]  # d(z4)/d(w4)=a3, shape: (4, 1)
         d_L_d_z4 = np.dot(d_L_d_y_hat, d_y_hat_d_z4)  # shape: (1, 1)
         d_L_d_w4 = np.dot(d_L_d_z4, d_z4_d_w4.T)  # shape: (1, 1) dot (1, 4) = (1, 4)
+        # d_z4_d_b4 = 1  # d(z4)/d(b4)=1, shape: (1)
+        # d_L_d_b4 = d_L_d_z4 * d_z4_d_b4  # shape: (1, 1)
 
-        # the graident of hidden layer 2: dL/d(a4) * d(a4)/d(z4) * d(z4)/d(a3) *
-        # d(a3)/d(z3) * d(z3)/d(w3) = dL/d(w3)
-        # dL/d(a4) * d(a4)/d(z4) = dL/d(z4)
-        d_z4_d_a3 = self.w4  # d(z4)/d(a3)=output_w  (a3 is the "x"), shape: (1, 4)
+        # the graident of hidden layer 2:
+        # dL/d(w3) = dL/d(z4) * d(z4)/d(a3) * d(a3)/d(z3) * d(z3)/d(w3)
+        # dL/d(b3) = dL/d(z4) * d(z4)/d(a3) * d(a3)/d(z3) * d(z3)/d(b3)
+        d_z4_d_a3 = self.w4  # d(z4)/d(a3)=w4  (a3 is the "x"), shape: (1, 4)
         d_a3_d_z3 = self.derivative_act(self.values["z3"])  # d(a3)/d(z3)=act'(z3), shape: (4, 1)
         d_z3_d_w3 = self.values["a2"]  # d(z3)/d(w3)=a2, shape: (4, 1)
         d_L_d_z3 = np.dot(np.dot(d_L_d_z4, d_z4_d_a3), d_a3_d_z3)  # shape: (1, 1) dot (1, 4) dot (4, 1) = (1, 1)
         d_L_d_w3 = np.dot(d_L_d_z3, d_z3_d_w3.T)  # shape: (1, 1) dot (1, 4) = (1, 4)
+        # d_z3_d_b3 = 1  # d(z3)/d(b3)=1, shape: (1)
+        # d_L_d_b3 = d_L_d_z3 * d_z3_d_b3  # shape: (1, 1)
 
-        # the graident of hidden layer 1: dL/d(a4) * d(a4)/d(z4) * d(z4)/d(a3) *
-        # d(a3)/d(z3) * d(z3)/d(a2) * d(a2)/d(z2) * d(z2)/d(w2) = dL/d(w2)
-        # dL/d(a4) * d(a4)/d(z4) * d(z4)/d(a3) * d(a3)/d(z3) = dL/d(z3)
+        # the graident of hidden layer 1:
+        # dL/d(w2) = dL/d(z3) * d(z3)/d(a2) * d(a2)/d(z2) * d(z2)/d(w2)
+        # dL/d(b2) = dL/d(z3) * d(z3)/d(a2) * d(a2)/d(z2) * d(z2)/d(b2)
         d_z3_d_a2 = self.w3  # d(z3)/d(a2)=w3 (a2 is the "x"), shape: (4, 4)
         d_a2_d_z2 = self.derivative_act(self.values["z2"])  # d(a2)/d(z2)=act'(z2), shape: (4, 1)
         d_z2_d_w2 = self.values["a1"]  # d(z2)/d(w2)=a1, shape: (4, 1)
         d_L_d_z2 = np.dot(d_L_d_z3 * d_z3_d_a2, d_a2_d_z2)  # shape: (1, 1) * (4, 4) dot (4, 1) = (4, 1)
         d_L_d_w2 = np.dot(d_L_d_z2, d_z2_d_w2.T)  # shape: (1, 1) dot (1, 4) = (1, 4)
+        # d_z2_d_b2 = 1  # d(z3)/d(b3)=1, shape: (1)
+        # d_L_d_b2 = d_L_d_z2 * d_z2_d_b2  # shape: (1, 4)
 
-        # the graident of input layer: dL/d(a4) * d(a4)/d(z4) * d(z4)/d(a3) *
-        # d(a3)/d(z3) * d(z3)/d(a2) * d(a2)/d(z2) * d(z2)/d(a1) * d(a1)/d(z1) *
-        # d(z1)/d(w1) = dL/d(w1)
-        # dL/d(a4) * d(a4)/d(z4) * d(z4)/d(a3) * d(a3)/d(z3) * d(z3)/d(a2) *
-        # d(a2)/d(z2) = dL/d(z2)
+        # the graident of input layer:
+        # dL/d(w1) = dL/d(z2) * d(z2)/d(a1) * d(a1)/d(z1) * d(z1)/d(w1)
+        # dL/d(b1) = dL/d(z2) * d(z2)/d(a1) * d(a1)/d(z1) * d(z1)/d(b1)
         d_z2_d_a1 = self.w2  # d(z2)/d(a1)=w2 (a1 is the "x"), shape: (4, 4)
         d_a1_d_z1 = self.derivative_act(self.values["z1"])  # d(a1)/d(z1)=act'(z1), shape: (4, 1)
         d_z1_d_w1 = self.values["x"]  # d(z1)/d(w1)=x, shape: (2, 1)
-        d_L_d_w1 = np.dot(
-            np.dot(np.dot(d_L_d_z2.T, d_z2_d_a1), d_a1_d_z1), d_z1_d_w1.T
-        )  # shape: (1, 4) dot (4, 4) dot (4, 1) dot (1, 2) = (1, 2)
+        d_L_d_z1 = np.dot(np.dot(d_L_d_z2.T, d_z2_d_a1), d_a1_d_z1)  # shape: (1, 4) dot (4, 4) dot (4, 1) = (1, 1)
+        d_L_d_w1 = np.dot(d_L_d_z1, d_z1_d_w1.T)  # shape: (1, 1)dot (1, 2) = (1, 2)
+        # d_z1_d_b1 = 1  # d(z1)/d(b1)=1, shape: (1)
+        # d_L_d_b1 = d_L_d_z1 * d_z1_d_b1  # shape: (1, 2)
 
-        # update weights
+        # update weights and biases
         self.w1 -= self.lr * d_L_d_w1
+        # self.b1 -= self.lr * d_L_d_b1
         self.w2 -= self.lr * d_L_d_w2
+        # self.b2 -= self.lr * d_L_d_b2
         self.w3 -= self.lr * d_L_d_w3
+        # self.b3 -= self.lr * d_L_d_b3
         self.w4 -= self.lr * d_L_d_w4
+        # self.b4 -= self.lr * d_L_d_b4
+
+
+def show_results(x, y, y_hat):
+    plt.subplot(1, 2, 1)
+    plt.title("Ground truth", fontsize=18)
+    for i in range(len(x)):
+        if y[i] == 0:
+            plt.plot(x[i][0], x[i][1], "ro")
+        else:
+            plt.plot(x[i][0], x[i][1], "bo")
+
+    plt.subplot(1, 2, 2)
+    plt.title("Predict result", fontsize=18)
+    for i in range(len(x)):
+        if y_hat[i] == 0:
+            plt.plot(x[i][0], x[i][1], "ro")
+        else:
+            plt.plot(x[i][0], x[i][1], "bo")
+
+    plt.show()
 
 
 def train_loop(model, inputs, labels, num_epochs):
@@ -209,18 +245,43 @@ def train_loop(model, inputs, labels, num_epochs):
             print(f"epoch {epoch} loss : {loss[0][0]}")
 
 
+def test(model, inputs, labels):
+    accuracy = 0
+    loss = 0
+    results = []
+    predictions = []
+
+    for i, (input, label) in enumerate(zip(inputs, labels)):
+        input = input.reshape(2, 1)
+        y_hat = model.forward(input)
+        results.append(y_hat[0][0])
+        predictions.append(np.round(y_hat))
+        loss += binary_cross_entropy(label, y_hat)
+        accuracy += 1 if np.round(y_hat) == label else 0
+        print(f"Iter{i} |   Ground truth: {label[0]:.1f} |   Prediction: {y_hat[0][0]:.5f} |")
+
+    print(results)
+
+    loss /= len(inputs)
+    accuracy /= len(inputs)
+    print(f"loss={loss[0][0]:.5f} accuracy={accuracy * 100:.2f}%")
+
+    show_results(inputs, labels, predictions)
+
+
 if __name__ == "__main__":
     # constants
     DATASET_SIZE = 100
-    NUM_EPOCHS = 100000
+    NUM_EPOCHS = 500
     ACTIVATION = "sigmoid"
-    LR = 1e-1
+    LR = 1e-3
 
-    # inputs, labels = generate_linear(DATASET_SIZE)
-    inputs, labels = generate_XOR_easy()
+    inputs, labels = generate_linear(DATASET_SIZE)
+    # inputs, labels = generate_XOR_easy()
 
     # create network
     net = Network(input_dim=2, hidden_dims=[4, 4, 4], output_dim=1, activation=ACTIVATION, lr=LR)
 
     # train network
     train_loop(net, inputs, labels, NUM_EPOCHS)
+    test(net, inputs, labels)
